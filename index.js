2c78f4afdd251ac45859c95842f0b4f6e2c22286
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2016-09-29 10:12:23 +0330

    added base metadb methods

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 "use strict";
 
 const task = require('xcane').task;
+const type = require('xcane').type;
+const Sequelize = require('sequelize');
 let Meta = null;
 
+/**
+ * @desc Provides a usable mechanism to store some metadata in form of
+ * key-value pairs in a relational database
+ * @author Mohamad mehdi Kharatizadeh - m_kharatizadeh@yahoo.com
+ */
 class SequelizeDbMeta {
+  /**
+   * @desc initialize model definitions in database
+   * @param {Sequelize} sequelize - a reference to an instance of Sequelize
+   */
   static init(sequelize) {
     Meta = sequelize.define('meta', {
       key: {
@@ -14,21 +25,96 @@ class SequelizeDbMeta {
         type: Sequelize.TEXT,
         allowNull: false
       }
+    }, {
+      timestamps: false
     });
   }
 
-  static _get(key) {
+  /**
+   * @desc internal function to query a single key from database
+   * @param {string} key - the key to requested value
+   * @param {*=} transaction - optional reference to transaction object
+   * @return {Promise.<*>} - value stored at key
+   */
+  static _get(key, transaction) {
     return task.spawn(function* () {
-      try {
-        return yield Meta.findOne({
-          where: {
-            key: key
-          }
-        });
-      } catch (err) {
-        console.log('ERRRRRRR:', err);
-        throw err;
+      let res = yield Meta.findOne(Object.assign({
+        where: {
+          key: key
+        },
+        attributes: ['value']
+      }, type.isOptional(transaction) ? null : {transaction: transaction}));
+
+      if (res === null) {
+        return {
+          value: null,
+          found: false
+        };
       }
+
+      return {
+        value: JSON.parse(res.value).value,
+        found: true
+      };
     });
   }
+
+  /**
+   * @desc get value stored at key or a default value
+   * @param {string} key - key to requested vaue
+   * @param {*} value - default value to use if key is not found
+   * @param {*=} transaction - optional sequelize transaction objet
+   * @return {Promise.<*>} - returns value stored at key or a default value
+   */
+  static getOrDefault(key, value, transaction) {
+    return SequelizeDbMeta._get(key, transaction).then(v => {
+      if (!v.found) {
+        return Promise.resolve(value);
+      }
+
+      return Promise.resolve(v.value);
+    });
+  }
+
+  /**
+   * @desc gets value stored at key or null value
+   * @param {string} key - key to requested value
+   * @param {*=} transaction - optional transaction object
+   * @return {Promise.<*|null>} - value stored at requested key
+   */
+  static getOrNull(key, transaction) {
+    return SequelizeDbMeta.getOrDefault(key, null, transaction);
+  }
+
+  /**
+   * @desc gets value stored at key or rejects with an error
+   * @param {string} key - key to requested value
+   * @param {*=} transaction - optional transaction object
+   * @return {Promise.<*>} - value stored at requested key
+   */
+  static get(key, transaction) {
+    return SequelizeDbMeta._get(key, transaction).then(v => {
+      if (!v.found) {
+        return Promise.reject(new Error(`key not found: ${key}`));
+      }
+
+      return Promise.resolve(v.value);
+    });
+  }
+
+  /**
+   * @desc sets value at target key
+   * @param {string} key - key to requested value
+   * @param {*} value - any javascript object to store
+   * @param {*=} transaction - optional sequelize transaction object
+   * @return {Promise} - resolve when value is created
+   */
+  static put(key, value, transaction) {
+    return Meta.create(Object.assign({
+      key: key,
+      value: JSON.stringify({value: value})
+    }, type.isOptional(transaction) ? null : {transaction: transaction}));
+  }
 }
+
+module.exports = SequelizeDbMeta;
--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const task = require('xcane').task;
 const metaDb =  require('../index');
 const expect = require('chai').expect;
 const Sequelize = require('sequelize');
@@ -10,7 +11,8 @@ describe('sequelize-db-meta', () => {
   before(done => {
     sequelize = new Sequelize({
       dialect: 'sqlite',
-      storage: ':memory:'
+      storage: ':memory:',
+      benchmark: true
     });
 
     metaDb.init(sequelize);
@@ -18,10 +20,85 @@ describe('sequelize-db-meta', () => {
     sequelize.sync().then(() => done()).catch(done);
   });
 
-  describe('#_get()', () => {
-    it('should not find if key does not exist', done => {
-      metaDb._get('sample_key').then(() => {
-        done();
+  describe('#put()', () => {
+    it('should correctly put value into storage', done => {
+      task.spawn(function* () {
+        yield metaDb.put('sample-key', 'a value');
+        expect(yield metaDb.get('sample-key')).to.be.equal('a value');
+      }).then(() => done()).catch(done);
+    });
+  });
+
+  describe('#getOrDefault()', () => {
+    it('should return default value if key does not exist', done => {
+      task.spawn(function* () {
+        expect(yield metaDb.getOrDefault('non-key', {a: 'default'}))
+          .to.be.deep.equal({a: 'default'});
+
+        expect(yield metaDb.getOrDefault('non-key', {a: 'default2'}))
+          .to.be.deep.equal({a: 'default2'});
+      }).then(() => done()).catch(done);
+    });
+
+    it('shoudl return value stored at db if key exists', done => {
+      task.spawn(function* () {
+        yield metaDb.put('my:key', 123);
+        expect(yield metaDb.getOrDefault('my:key', 'def'))
+          .to.be.equal(123);
+      }).then(() => done()).catch(done);
+    });
+  });
+
+  describe('#getOrNull()', () => {
+    it('should return null value if key does not exist', done => {
+      task.spawn(function* () {
+        expect(yield metaDb.getOrNull('non-key'))
+          .to.be.null;
+      }).then(() => done()).catch(done);
+    });
+
+    it('should return value stored at db if key exists', done => {
+      task.spawn(function* () {
+        yield metaDb.put('qwe:eee', 'just some value');
+        expect(yield metaDb.getOrNull('qwe:eee'))
+          .to.be.equal('just some value');
+      }).then(() => done()).catch(done);
+    });
+  });
+
+  describe('#get()', () => {
+    it('should throw error if key does not exist', done => {
+      metaDb.get('non-key')
+        .then(() => done('it should not had passed'))
+        .catch(err => {
+          expect(err).to.be.an.instanceof(Error);
+          expect(err.message).to.be.equal('key not found: non-key');
+          done();
+        }).catch(done);
+    });
+
+    it('should return correct value if key exists', done => {
+      task.spawn(function* () {
+        yield metaDb.put('the king', 'is here');
+        expect(yield metaDb.get('the king')).to.be.equal('is here');
+      }).then(() => done()).catch(done);
+    });
+  });
+
+  describe('#transactionManagement', () => {
+    it('should manage transaction correctly', done => {
+      sequelize.transaction(t => task.spawn(function* () {
+        yield metaDb.put('my-tran-key', 'my-value');
+        expect(yield metaDb.get('my-tran-key')).to.be.equal('my-value');
+        throw new Error('oops');
+      })).then(() => done('it should not had succeeded')).catch(err => {
+        expect(err).to.be.an.instanceof(Error);
+        expect(err.message).to.be.equal('oops');
+
+        task.spawn(function* () {
+          expect(yield metaDb.getOrDefault('my-tran-key', 'default'))
+            .to.be.equal('default');
+        }).then(() => done()).catch(done);
       }).catch(done);
     });
   });
